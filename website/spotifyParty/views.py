05c0be5da9df217cd6e1ac6fa3ccfd9799f571ca
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2020-12-17 17:04:43 +0100

    add basic view and html for index

--- website/spotifyParty/views.py
+++ website/spotifyParty/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+
+from .models import PartySession
 
 
 def index(request):
-    pass
+    if request.method == 'POST':
+        submitted_session_code = request.POST.get('session_code')
+        active_party_session = PartySession.objects.filter(session_code=submitted_session_code)
+        if active_party_session:
+            return render(request, 'index.html', {'error_msg': 'Your PartySession was found in the database. Unfortunately live Sessions are not yet implemented'})
+        return render(request, 'index.html', {'error_msg': 'Sorry, no matching PartySession was found.'})
+    return render(request, 'index.html', {'error_msg': ''})
 
 
 def settings(request):
     pass
 
 
-# view to be implemented by DjangoChannels
+# view to be implemented with DjangoChannels
 def party_session(request):
     pass
